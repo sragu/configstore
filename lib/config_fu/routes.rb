1133bd305532964166d50e70db1a63c6b13fc64f
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2013-07-25 00:25:09 +0100

    using reverseloop to identify the client

--- lib/config_fu/routes.rb
+++ lib/config_fu/routes.rb
@@ -1,4 +1,9 @@
+require 'resolv'
+
 get '/config' do
   content_type :json
-  { :name => "appname", :db_url => "db.hostname.com/db/name"}.to_json
+
+  { :name => "appname", 
+    :identified_as => Resolv.new.getname(request.ip),
+    :db_url => "db.hostname.com/db/name"}.to_json
 end
