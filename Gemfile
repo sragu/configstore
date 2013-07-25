97ae411da79fddf13c1540fc5d60b7dafcebc2b7
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2013-07-25 10:24:22 +0100

    support http header to override identity from reverse dns loopkup
    for testing

--- Gemfile
+++ Gemfile
@@ -3,3 +3,8 @@ source "https://rubygems.org"
 
 gem 'sinatra'
 gem 'json'
+
+
+group :development do 
+  gem 'pry' 
+end
--- lib/config_fu/routes.rb
+++ lib/config_fu/routes.rb
@@ -1,9 +1,10 @@
 require 'resolv'
 
 get '/config' do
-  content_type :json
+  src = request.env['HTTP_X_IDENTITY'] || Resolv.new.getname(request.ip)
 
+  content_type :json
   { :name => "appname", 
-    :identified_as => Resolv.new.getname(request.ip),
+    :identified_as => src,
     :db_url => "db.hostname.com/db/name"}.to_json
 end
